acdc7610d0cffe4fd174762acc52f5e7fe9f2127
GitHubLim <ponde11311@gmail.com>
2017-07-16 17:00:51 +0900

    170706 modified

--- main.cpp
+++ main.cpp
@@ -19,15 +19,46 @@ int main(void){
 	video.set(CAP_PROP_FRAME_WIDTH, 320);
 	video.set(CAP_PROP_FRAME_HEIGHT, 240);
 
-	Mat frame, houghFrame;
+	Mat frame, houghFrame, kmeansFrame, ppFrame;
+	int flag = 0;
 
-	//Main Loop
+	//HSV Trackbar
+	namedWindow("찾을 색범위 설정", CV_WINDOW_AUTOSIZE);
+
+	//트랙바에서 사용되는 변수 초기화 
+	int LowH = 170;
+	int HighH = 179;
+
+	int LowS = 50;
+	int HighS = 255;
+
+	int LowV = 0;
+	int HighV = 255;
+
+	//트랙바 생성 
+	cvCreateTrackbar("LowH", "찾을 색범위 설정", &LowH, 179); //Hue (0 - 179)
+	cvCreateTrackbar("HighH", "찾을 색범위 설정", &HighH, 179);
+
+	cvCreateTrackbar("LowS", "찾을 색범위 설정", &LowS, 255); //Saturation (0 - 255)
+	cvCreateTrackbar("HighS", "찾을 색범위 설정", &HighS, 255);
+
+	cvCreateTrackbar("LowV", "찾을 색범위 설정", &LowV, 255); //Value (0 - 255)
+	cvCreateTrackbar("HighV", "찾을 색범위 설정", &HighV, 255);
+
+	//MAIN LOOP
 	while(true){ 
 		video >> frame;
 
-		houghFrame = houghLine(frame);
-		imshow("PMS", houghFrame);
+		//FPS 
+		string fps = to_string((int)video.get(CV_CAP_PROP_FPS));
+		putText(frame, "FPS : " + fps, Point(10, 40), 2, 1.2, Scalar(0, 255, 0));
 
+		//houghFrame = houghLine(frame);
+		//kmeansFrame = kmeansClustering(frame);
+		//ppFrame = preprocessing(frame);
+		imshow("PMS", frame);
+
+		//Pushing esc key is terminate
 		if (waitKey(DELAYMILL) == ESC)
 			break; 
 	}
--- pms.h
+++ pms.h
@@ -4,16 +4,20 @@
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <time.h>
 
 #define FILENAME "rainy1.mp4"
 #define ESC 27
 #define DELAYMILL 20
 #define PI 3.14159265
 #define DEG2RAD PI/180
+#define K 2
 
 using namespace std;
 using namespace cv;
 
+
+//DO NOT USE FUNCTION
 Mat houghLine(Mat srcFrame) {
 	//Exception handling (Image is Empty)
 	if (srcFrame.empty() == true) {
@@ -27,7 +31,7 @@ Mat houghLine(Mat srcFrame) {
 	//1. GaussianBular and Canny Edge Detection
 	Mat edgeFrame;
 	GaussianBlur(grayFrame, grayFrame, Size(9, 9), 1.0);
-	Canny(grayFrame, edgeFrame, 100, 150);
+	Canny(grayFrame, edgeFrame, 150, 350);
 
 
 	//2. Mapping of edge points to the Hough space and storage in an accumulator
@@ -35,5 +39,81 @@ Mat houghLine(Mat srcFrame) {
 	HoughLinesP()*/
 	return edgeFrame;
 }
+Mat kmeansClustering(Mat srcFrame) {
+	
+	//배열에 8bit uchar type으로 저장되어 있음.
+	Mat colorTable(K, 1, CV_8UC3);
+	Vec3b color;
 
+
+	//destination 이미지 만들기
+	Mat dstFrame(srcFrame.size(), srcFrame.type());
+
+	//SIZE 추출
+	cout << "SIZE : " << srcFrame.size() << endl;
+
+	int attempts = 1;
+	int flags = KMEANS_RANDOM_CENTERS;
+	TermCriteria criteria(TermCriteria::COUNT + TermCriteria::EPS, 10, 1.0);
+
+	Mat samples = srcFrame.reshape(3, srcFrame.rows*srcFrame.cols);
+	cout << samples.size() << endl;
+
+	samples.convertTo(samples, CV_32FC3);
+
+	Mat labels;
+	srcFrame.convertTo(srcFrame, CV_32FC3);
+
+	kmeans(samples, K, labels, criteria, attempts, flags);
+
+	//output size
+	cout << labels.size() << endl;
+
+	// 시간측정
+	clock_t begin, end;
+
+	begin = clock();
+
+	for (int y = 0, n = 0; y < srcFrame.rows; y++) {
+		for (int x = 0; x < srcFrame.cols; x++, n++) {
+			int idx = labels.at<int>(n);
+			//idx로 추출함 (idx = 정수배열)
+			color = colorTable.at<Vec3b>(idx);
+			dstFrame.at<Vec3b>(y, x) = color;
+		}
+	}
+
+	cout << srcFrame.size() << endl;
+	end = clock();
+
+	cout << "K-means 수행시간 : " << (float)(end - begin) / CLOCKS_PER_SEC << endl;
+
+	return dstFrame;
+}
+
+double calculateFPS() {
+	// method 2
+	double fps = 0;
+	int64 t = getTickCount(); //측정 시작 시간
+
+    // 측정 필요한 구간
+	t = getTickCount() - t; //측정 완료 시간
+	fps = 1000 / t / getTickFrequency(); // fps 계산
+
+	return fps;
+}
+
+Mat preMasking(Mat srcFrame) {
+	return srcFrame;
+}
+
+Mat preprocessing(Mat srcFrame){
+	Mat dstFrame, mask;
+
+	//1. masking 
+	mask = preMasking(srcFrame);
+	srcFrame.copyTo(dstFrame, mask);
+
+	return dstFrame;
+}
 #endif
